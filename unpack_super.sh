23d27b5a7c8adb2f448c312658fe2eafa345ffcf
RiskyGUY22 <thomas_turner@talktalk.net>
2024-03-16 19:43:09 +0000

    Added extraction of super image

--- unpack_super.sh
+++ unpack_super.sh
@@ -28,6 +28,21 @@ if [ -e "$super_img_path/super.img" ]; then
     echo "Super.img located!"
     echo "Copying to CRB..."
     cp -f $super_img_path/super.img ./Projects/$project_option/Input/
+
+    echo ""
+    echo "Extracting Dynamic Partitions..."
+    cd Projects/$project_option/Input/
+    simg2img super.img super.img.raw
+    echo "Extracting system image..."
+    ./../../../tools/super_to_raw/lpunpack --partition=system super.img.raw
+    echo "Extracting product image..."
+    ./../../../tools/super_to_raw/lpunpack --partition=product super.img.raw
+    echo "Extracting vendor image..."
+    ./../../../tools/super_to_raw/lpunpack --partition=vendor super.img.raw
+    echo "Extracting odm image..."
+    ./../../../tools/super_to_raw/lpunpack --partition=odm super.img.raw
+    echo ""
+    echo "Done!"
 else
     echo "Incorrect path specified!"
     echo "Check a super.img is present in the directory!"
